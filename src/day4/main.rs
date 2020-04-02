cd6e440d39edcf5ebbd0ba21d7831310456d5dd2
Sven Niederberger <s-niederberger@outlook.com>
2020-04-02 15:46:47 +0200

    .

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -1,6 +1,6 @@
-fn check_criteria(candidate: u32) -> bool {
+fn get_digits(number: u32) -> Vec<u32> {
     let mut digits = Vec::new();
-    let mut n = candidate;
+    let mut n = number;
     while n > 9 {
         digits.push(n % 10);
         n = n / 10;
@@ -8,21 +8,61 @@ fn check_criteria(candidate: u32) -> bool {
     digits.push(n);
     digits.reverse();
 
-    let mut ok = true;
+    digits
+}
 
-    ok &= digits.len() == 6;
-
-    // any sequence of 2
-    ok &= digits[0..digits.len() - 1]
+fn is_ascending_order(digits: &Vec<u32>) -> bool {
+    digits[0..digits.len() - 1]
         .iter()
         .zip(&digits[1..])
-        .any(|(a, b)| a == b);
+        .all(|(a, b)| a <= b)
+}
 
-    // ascending order
-    ok &= digits[0..digits.len() - 1]
+fn has_double_sequence(digits: &Vec<u32>) -> bool {
+    // any sequence of 2
+    digits[0..digits.len() - 1]
         .iter()
-        .zip(&digits[1..digits.len()])
-        .all(|(a, b)| a <= b);
+        .zip(&digits[1..])
+        .any(|(a, b)| a == b)
+}
+
+fn has_double_sequence_strict(digits: &Vec<u32>) -> bool {
+    let mut has_double_sequence = false;
+    for i in 1..digits.len() {
+        if digits[i - 1] == digits[i] {
+            let mut not_triple_sequence = true;
+            if i != 1 {
+                if digits[i - 2] == digits[i - 1] {
+                    not_triple_sequence = false;
+                }
+            }
+            if i != 5 {
+                if digits[i] == digits[i + 1] {
+                    not_triple_sequence = false;
+                }
+            }
+            if not_triple_sequence {
+                has_double_sequence = true;
+            }
+        }
+    }
+    has_double_sequence
+}
+
+fn check_criteria_pt1(digits: &Vec<u32>) -> bool {
+    let mut ok = true;
+    ok &= digits.len() == 6;
+    ok &= has_double_sequence(&digits);
+    ok &= is_ascending_order(&digits);
+
+    ok
+}
+
+fn check_criteria_pt2(digits: &Vec<u32>) -> bool {
+    let mut ok = true;
+    ok &= digits.len() == 6;
+    ok &= has_double_sequence_strict(&digits);
+    ok &= is_ascending_order(&digits);
 
     ok
 }
@@ -31,13 +71,20 @@ fn main() {
     let range_start = 125730;
     let range_end = 579381;
 
-    let mut viable = Vec::new();
+    let mut num_viable_pt1 = 0;
+    let mut num_viable_pt2 = 0;
 
     for candidate in range_start..=range_end {
-        if check_criteria(candidate) {
-            viable.push(candidate)
+        let digits = get_digits(candidate);
+
+        if check_criteria_pt1(&digits) {
+            num_viable_pt1 += 1;
+        }
+        if check_criteria_pt2(&digits) {
+            num_viable_pt2 += 1;
         }
     }
 
-    println!("Solution Part 1: {}", viable.len());
+    println!("Solution Part 1: {}", num_viable_pt1);
+    println!("Solution Part 2: {}", num_viable_pt2);
 }
